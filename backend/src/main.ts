c3e6aa458921e6b7a36438e76ba7f1b8f83b4f36
SyrtcevaDaria <syrtceva566333@gmail.com>
2024-11-21 11:04:53 +0300

    fix: change cors rules

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,15 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://127.0.0.1:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: 'http://127.0.0.1:3000',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  // });
+
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
